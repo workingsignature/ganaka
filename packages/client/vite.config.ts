1ee7b3d6c92a19958f9b98c87fcfa215e7593894
shimorojune <rohitrajnovember@gmail.com>
2025-09-24 21:18:28 +0530

    Shift to normal react app

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,25 +1,7 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
-
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
-    tailwindcss(),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
+  plugins: [react()],
 })
