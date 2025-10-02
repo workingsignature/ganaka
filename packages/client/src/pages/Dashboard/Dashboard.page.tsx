4d9fb239c40219236815629680b1002b1449c6b7
shimorojune <rohitrajnovember@gmail.com>
2025-10-02 09:25:12 +0530

    Pre-move to mantine UI

--- packages/client/src/pages/Dashboard/Dashboard.page.tsx
+++ packages/client/src/pages/Dashboard/Dashboard.page.tsx
@@ -3,6 +3,7 @@ import { BotInfoCard } from "./components/BotInfoCard/BotInfoCard";
 import { DateSelector } from "./components/DateSelector";
 import { ShortlistCard } from "./components/ShortlistCard/ShortlistCard";
 import { useQueryParams } from "@/utils/hooks/useQueryParams";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export const DashboardPage = () => {
   // HOOKS
@@ -17,30 +18,65 @@ export const DashboardPage = () => {
         <DateSelector />
         <ShortlistCard />
       </div>
-      <div className="w-full h-full grid grid-rows-[1fr_3fr] gap-2">
-        <Card className="w-full h-full">
-          <CardHeader>
-            <CardTitle>Statistics</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p>
-              Here are your top recommendations for the week. These are based on
-              your preferences and the weather.
-            </p>
-          </CardContent>
-        </Card>
-        <Card className="w-full h-full">
-          <CardHeader>
-            <CardTitle>Charts</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p>
-              Here are your top recommendations for the week. These are based on
-              your preferences and the weather.
-            </p>
-          </CardContent>
-        </Card>
-      </div>
+
+      <Tabs defaultValue="charts">
+        <TabsList>
+          <TabsTrigger value="charts">Overview</TabsTrigger>
+          <TabsTrigger value="statistics">Statistics</TabsTrigger>
+        </TabsList>
+        <TabsContent value="charts">
+          <div className="w-full h-full grid grid-rows-[1fr_3fr] gap-2">
+            <Card className="w-full h-full">
+              <CardHeader>
+                <CardTitle>Statistics</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>
+                  Here are your top recommendations for the week. These are
+                  based on your preferences and the weather.
+                </p>
+              </CardContent>
+            </Card>
+            <Card className="w-full h-full">
+              <CardHeader>
+                <CardTitle>Charts</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>
+                  Here are your top recommendations for the week. These are
+                  based on your preferences and the weather.
+                </p>
+              </CardContent>
+            </Card>
+          </div>
+        </TabsContent>
+        <TabsContent value="statistics">
+          <div className="w-full h-full grid grid-rows-[1fr_3fr] gap-2">
+            <Card className="w-full h-full">
+              <CardHeader>
+                <CardTitle>Statistics</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>
+                  Here are your top recommendations for the week. These are
+                  based on your preferences and the weather.
+                </p>
+              </CardContent>
+            </Card>
+            <Card className="w-full h-full">
+              <CardHeader>
+                <CardTitle>Charts</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>
+                  Here are your top recommendations for the week. These are
+                  based on your preferences and the weather.
+                </p>
+              </CardContent>
+            </Card>
+          </div>
+        </TabsContent>
+      </Tabs>
       <div className="w-full h-full">
         <BotInfoCard />
       </div>
